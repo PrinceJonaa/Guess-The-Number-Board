856e4f9919895b326d2fc04ecaecbe537d888934
princejonaa <jonathanbonner128@gmail.com>
2022-02-14 18:04:18 -0500

    edit some code

--- src/js/app.js
+++ src/js/app.js
@@ -29,36 +29,57 @@ const guessList2 = document.getElementById('guessList2')
 const StartGame = document.getElementById('startGameBtn')
 const guessEntryArea = document.getElementById('guessEntryForm')
 const resetBtn = document.getElementById('resetBtn')
+const guessNumber = document.getElementById('guessNumber')
+
+//=========================
+
 
 
 //=========================
-
-startbtn.addEventListener("click", boardLocation);
-
-//=========================
+function clickButton() {
+  startbtn.addEventListener("click", boardLocation);
+}
 
 function boardLocation() {
   window.location.href = "/board.html";
 }
 
 function guessTheNumber() {
-  var randomNumber = Math.floor(Math.random() * 100) + 1;
-  var guess = document.getElementById("guess").value;
-  var guessNumber = parseInt(guess);
-  var guessCount = 0;
-  var guessList = document.getElementById("guessList");
-  var guessListItem = document.createElement("li");
-  var guessListText = document.createTextNode(guess);
-  guessListItem.appendChild(guessListText);
-  guessList.appendChild(guessListItem);
-  guessCount++;
-  if (guessNumber === randomNumber) {
-    alert("You Win!");
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  let guess = guessNumber.value;
+  if (randomNumber == guess)
+  {
+    alert ("you won")
   }
-  else if (guessCount === 5) {
-    alert("You Lose!");
+  else if (guess > randomNumber)
+  {
+    guess++;
+    alert('small number')
   }
-  else {
-    guessTheNumber();
+  else
+  {
+    guess++;
+    alert('greater number')
   }
 }
+// function guessTheNumber() {
+//   var randomNumber = Math.floor(Math.random() * 100) + 1;
+//   var guess = document.getElementById("guess").value;
+//   var guessNumber = parseInt(guess);
+//   var guessCount = 0;
+//   var guessList = document.getElementById("guessList");
+//   var guessListItem = document.createElement("li");
+//   var guessListText = document.createTextNode(guess);
+//   guessListItem.appendChild(guessListText);
+//   guessList.appendChild(guessListItem);
+//   guessCount++;
+//   if (guessNumber === randomNumber) {
+//     alert("You Win!");
+//   }
+//   else if (guessCount === 5) {
+//     alert("You Lose!");
+//   }
+//   else {
+//     guessTheNumber();
+//   }
+// }
