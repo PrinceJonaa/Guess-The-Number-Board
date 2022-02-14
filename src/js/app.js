5f76d57ae9fcc16b0c28753110c5dcde1f34afd4
princejonaa <jonathanbonner128@gmail.com>
2022-02-13 21:38:42 -0500

    added temp guess the number game

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,25 @@ startbtn.addEventListener("click", boardLocation);
 function boardLocation() {
   window.location.href = "/board.html";
 }
+
+function guessTheNumber() {
+  var randomNumber = Math.floor(Math.random() * 100) + 1;
+  var guess = document.getElementById("guess").value;
+  var guessNumber = parseInt(guess);
+  var guessCount = 0;
+  var guessList = document.getElementById("guessList");
+  var guessListItem = document.createElement("li");
+  var guessListText = document.createTextNode(guess);
+  guessListItem.appendChild(guessListText);
+  guessList.appendChild(guessListItem);
+  guessCount++;
+  if (guessNumber === randomNumber) {
+    alert("You Win!");
+  }
+  else if (guessCount === 5) {
+    alert("You Lose!");
+  }
+  else {
+    guessTheNumber();
+  }
+}
